af6a5604c97643f0024d8a47b43bbc5052fa7308
untrots <untrots@gmail.com>
2015-09-30 17:17:20 +0300

    LedStrip: Set all leds to black upon creation

--- projects/MadHat_ar/LedStrip.cpp
+++ projects/MadHat_ar/LedStrip.cpp
@@ -14,6 +14,10 @@ void LedStrip::init(int length) {
     _length = length;
     _data = new CRGB[length];
     FastLED.addLeds<WS2811, DATA_PIN, RGB>(_data, length);
+
+    for(int i = 0; i < _length; i++) {
+        _data[i] = CRGB::Black;
+    }
 }
 
 void LedStrip::sendFrame() {
