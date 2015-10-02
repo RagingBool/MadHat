50f24b10f3a3d06288d26e956ec68d85ec280ff6
untrots <untrots@gmail.com>
2015-10-02 15:16:44 +0300

    MadHat: head lights switches between several effects

--- projects/MadHat_ar/MadHat.cpp
+++ projects/MadHat_ar/MadHat.cpp
@@ -8,12 +8,12 @@ void MadHat::init() {
     _ledStrip.init(14);
     Light testLight(_ledStrip, 1);
 
-    /*
+    
     int headLeds[] = { 0, 1, 2, 3, 4, 5 };
     _pHeadLight = new Light(_ledStrip, headLeds, 6);
     int eyesLeds[] = { 6, 7, 8, 9 };
     _pEyesLight = new Light(_ledStrip, eyesLeds, 4);
-    */
+    
 
     /*
     int headLeds[] = { 0, 1, 2, 3, 5, 7, 8, 9 };
@@ -22,9 +22,11 @@ void MadHat::init() {
     _pEyesLight = new Light(_ledStrip, eyesLeds, 2);
     */
 
+    /*
     int headLeds[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
-    _pHeadLight = new Light(_ledStrip, headLeds, 143);
+    _pHeadLight = new Light(_ledStrip, headLeds, 14);
     _pEyesLight = new Light(_ledStrip, 0, 0);
+    */
     
     initHeadFx();
     initEyesFx();
@@ -33,10 +35,15 @@ void MadHat::init() {
 }
 
 void MadHat::initHeadFx() {
-    IFx* pCyclingColors = new ColorCycleFx(_pHeadLight, 30000, .7f, 0.7f);
-    IFx* pCrazyColors = new CrazyLightsFx(_pHeadLight, 1.f, 1.f);
+    IFx* pCrazyColors = new CrazyLightsFx(_pHeadLight, 1.f, 0.7f);
 
-    _pHeadFx = new InterruptingFx(pCyclingColors, pCrazyColors, 10000, 20000, 1000, 2000);
+    IFx** ppMainFxs = new IFx*[3];
+    ppMainFxs[0] = new ColorCycleFx(_pHeadLight, 1000, .7f, 0.7f);
+    ppMainFxs[1] = new ChasingLightsFx(_pHeadLight, 100, 1, 4, 1.f, 1.f);
+    ppMainFxs[2] = new ChasingLightsFx(_pHeadLight, 350, 5, 8, .7f, .5f);
+    IFx* pSwtichingFx = new SwitchingFx(ppMainFxs, 3, 5000, 10000);
+
+    _pHeadFx = new InterruptingFx(pSwtichingFx, pCrazyColors, 10000, 20000, 1000, 2000);
 }
 
 void MadHat::initEyesFx() {
